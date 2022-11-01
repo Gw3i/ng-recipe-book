b8c7eaa604b3ddf134599eadb8a5390ef504202f
Gw3i <contact.johngoldco@gmail.com>
2022-11-01 08:35:42 +0100

    Add ingredients in form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
 
-  constructor() { }
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
 
-  ngOnInit(): void {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }

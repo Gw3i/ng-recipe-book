20ec249b3ca290fed009892d542f005b1439464d
Gw3i <contact.johngoldco@gmail.com>
2022-11-01 08:56:20 +0100

    Update Directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,10 +8,12 @@ import {
 @Directive({
   selector: '[appDropdown]',
 })
-export class DropdowmDirective {
+export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+  constructor(private elRef: ElementRef) {}
 }

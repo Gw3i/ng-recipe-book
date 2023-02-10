7d45207b8da57adac3a58251a22a19fdfbed246f
Gw3i <contact.johngoldco@gmail.com>
2023-02-10 20:35:54 +0100

    Add loading spinner and error to template

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -10,6 +10,8 @@ export class AuthComponent {
   constructor(private authService: AuthService) {}
 
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -23,15 +25,22 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
+
     if (this.isLoginMode) {
       // ...
     } else {
-      this.authService.signup(email, password).subscribe((responseData) => {
-        console.log(responseData),
-          (error) => {
-            console.log(error);
-          };
-      });
+      this.authService.signup(email, password).subscribe(
+        (responseData) => {
+          console.log(responseData);
+          this.isLoading = false;
+        },
+        (error) => {
+          console.log(error);
+          this.error = 'An error occured';
+          this.isLoading = false;
+        }
+      );
     }
 
     form.reset();
